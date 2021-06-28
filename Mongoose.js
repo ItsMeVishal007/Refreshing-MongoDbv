95fbe46ecbc048e6c7a57e392ceebfd5c00dcba3
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-06-28 17:38:06 +0530

    changes

--- Mongoose.js
+++ Mongoose.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+mongoose.connect(`mongodb+srv://drcyberx:drcyberx@cluster0.yyra3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,{ useUnifiedTopology: true, useNewUrlParser: true }).then(()=> {
+  console.log('db connected')
+}).catch(()=> {
+  console.log('connection failed')
+});
+
 const Product = require('./models/products');
 
 const createProduct = async (req, res, next) => {
@@ -7,6 +13,10 @@ const createProduct = async (req, res, next) => {
     name: req.body.name,
     price: req.body.price
   })
+
+  const result = await createdProduct.save();
+
+  res.status(201).json(result);
 }
 
 const getProducts = async (req, res, next) => {
